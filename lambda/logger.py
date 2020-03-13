fb4b77f6bc580d88fc710cc7ca0c1775b47e82ca
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2020-03-13 13:23:27 +0000

    Add clean before make checks to stop running against .target temp folder. Refactor logger to log in JSON so it's parsed properly in Splunk.

--- lambda/logger.py
+++ lambda/logger.py
@@ -1,9 +1,47 @@
 """ Declare a logger to be used by any module """
-import logging
 import os
+import sys
+import datetime
+import json
+import logging
 
-LOG = logging.getLogger("github_usage")
-STDERRLOGGER = logging.StreamHandler()
-STDERRLOGGER.setFormatter(logging.Formatter(logging.BASIC_FORMAT))
-LOG.addHandler(STDERRLOGGER)
-LOG.setLevel(getattr(logging, str(os.getenv("LOG_LEVEL", "ERROR"))))
+
+class JsonFormatter(logging.Formatter):
+    """ Handle log invokes with string, dict or json.dumps """
+    def format(self):
+        """ Detect formatting of self message and encode as valid JSON """
+        data = {}
+        data.update(vars(self))
+        try:
+            json.loads(self.msg)
+            parsed = json.loads(self.msg)
+            if type(parsed) in [dict, list]:
+                data["msg"] = parsed
+        except ValueError:
+            pass
+
+        try:
+            if ("args" in data) and len(data["args"]) > 0:
+                args = data["args"]
+                data["msg"] = data["msg"] % args
+        except TypeError:
+            pass
+
+        data["timestamp"] = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+
+        return json.dumps(data)
+
+
+def build_logger(log_name, log_level="ERROR"):
+    """ Create shared logger and custom JSON handler """
+    logger = logging.getLogger(log_name)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(JsonFormatter)
+    logger.handlers = []
+    logger.addHandler(handler)
+    logger.setLevel(getattr(logging, log_level))
+    return logger
+
+
+LOG_LEVEL = str(os.getenv("LOG_LEVEL", "ERROR"))
+LOG = build_logger("github_usage", log_level=LOG_LEVEL)
