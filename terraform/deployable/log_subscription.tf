0f7d4fe527c168b75024a0be9df5cc38f027980a
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2020-03-13 15:34:27 +0000

    Switch module ref to merged master commit

--- terraform/deployable/log_subscription.tf
+++ terraform/deployable/log_subscription.tf
@@ -1,5 +1,5 @@
 module "log_subscription" {
-  source = "git::ssh://git@github.com/alphagov/cyber-security-terraform//modules/csls_cloudwatch_log_group_subscription?ref=eng-222"
+  source = "git::ssh://git@github.com/alphagov/cyber-security-terraform//modules/csls_cloudwatch_log_group_subscription?ref=c1636d1"
   log_groups = {
     "/aws/lambda/github_usage_lambda": "INFO"
   }
