02bfba226ff190c2fc16933bfc0ad19ec076847b
alice-carr <alice.bc.carr@gmail.com>
2021-09-08 16:02:30 +0100

    call module to build ssh

---
+++ terraform/modules/codepipeline/module-codebuild-build-ssh-config.tf
@@ -0,0 +1,11 @@
+module "codebuild-build-ssh-config" {
+  source                      = "github.com/alphagov/cyber-security-shared-terraform-modules//codebuild/codebuild_build_ssh_config?ref=ce-266"
+  codebuild_service_role_name = var.codebuild_service_role_name
+  codebuild_image             = var.codebuild_image
+  pipeline_name               = var.pipeline_name
+  stage_name                  = "BuildSshConfig"
+  action_name                 = "CreateConfig"
+  environment                 = var.environment
+  deploy_key                  = var.deploy_key
+  tags                        = local.tags
+}
