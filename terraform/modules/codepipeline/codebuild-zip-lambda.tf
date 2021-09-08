52ddb8ab239d435d5971ebc82879bbc61330c2b5
alice-carr <alice.bc.carr@gmail.com>
2021-09-08 16:01:38 +0100

    simplify artifact output

--- terraform/modules/codepipeline/codebuild-zip-lambda.tf
+++ terraform/modules/codepipeline/codebuild-zip-lambda.tf
@@ -8,16 +8,7 @@ resource "aws_codebuild_project" "codebuild_zip_lambda" {
     type = "CODEPIPELINE"
 
   }
-
-  secondary_artifacts {
-    type                = "S3"
-    # name                = "github_usage_lambda"
-    packaging           = "ZIP"
-    artifact_identifier = "lambda_zip"
-    location            = data.aws_s3_bucket.artifact_store.bucket
-    path                = var.output_artifact_path
-  }
-
+  
   cache {
     type  = "LOCAL"
     modes = ["LOCAL_DOCKER_LAYER_CACHE", "LOCAL_SOURCE_CACHE"]
