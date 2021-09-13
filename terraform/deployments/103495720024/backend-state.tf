41ca8ea0f42f03fdb8078e8d71cb8622e5893227
alice-carr <alice.bc.carr@gmail.com>
2021-09-13 14:13:57 +0100

    add correct account id

--- terraform/deployments/103495720024/backend-state.tf
+++ terraform/deployments/103495720024/backend-state.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "gds-security-terraform"
-    key     = "terraform/state/account/779799343306/service/github-usage.tfstate"
+    key     = "terraform/state/account/103495720024/service/github-usage.tfstate"
     region  = "eu-west-2"
     encrypt = true
   }
