62ceb8d1e252df383054ae11ef87ff6e862f24d4
alice-carr <alice.bc.carr@gmail.com>
2021-09-14 11:35:07 +0100

    remove comments

--- terraform/deployments/670214072732/module_codepipeline.tf
+++ terraform/deployments/670214072732/module_codepipeline.tf
@@ -1,8 +1,8 @@
 module "codepipeline" {
   source                      = "../../modules/codepipeline"
   codestar_connection_id      = "51c5be90-8c8f-4d32-8be4-18b8f05c802c"
-  # codebuild_service_role_name = "CodePipelineExecutionRole"
-  # environment                 = "prod"
-  # docker_hub_credentials      = "docker_hub_credentials"
-  # github_pat                  = "/github/pat"
+  codebuild_service_role_name = "CodePipelineExecutionRole"
+  environment                 = "prod"
+  docker_hub_credentials      = "docker_hub_credentials"
+  deploy_key                  = "/github/deploy-keys/cyber-security-terraform"
 }
