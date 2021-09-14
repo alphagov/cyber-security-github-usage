5354fb81176ff428a5b20c72fce6ad6539072775
alice-carr <alice.bc.carr@gmail.com>
2021-09-14 10:17:58 +0100

    add module for terraform apply prod

---
+++ terraform/modules/codepipeline/module-codebuild-apply-terraform-prod.tf
@@ -0,0 +1,26 @@
+module "codebuild-apply-teraform-prod" {
+  source                      = "github.com/alphagov/cyber-security-shared-terraform-modules//codebuild/codebuild_apply_terraform"
+  codebuild_service_role_name = var.codebuild_service_role_name
+  deployment_account_id       = var.prod_account_id
+  deployment_role_name        = "CodePipelineDeployerRole_${var.prod_account_id}"
+  terraform_directory         = "terraform/deployments/${var.prod_account_id}"
+  codebuild_image             = var.codebuild_image
+  pipeline_name               = var.pipeline_name
+  stage_name                  = "TerraformApplyProd"
+  action_name                 = "TerraformApply"
+  environment                 = var.environment
+  docker_hub_credentials      = var.docker_hub_credentials
+  tags                        = local.tags
+  copy_artifacts              = [
+    { 
+      artifact = "ssh_config", 
+      source = ".ssh", 
+      target = "/root/.ssh"
+    }, 
+    { 
+      artifact = "lambda_zip", 
+      source = "github_usage_lambda.zip", 
+      target = "lambda"
+    }
+  ]
+}
