6f3e0a7a64f615a68d55ac930f7fe09906e49f7d
alice-carr <alice.bc.carr@gmail.com>
2021-09-14 11:37:07 +0100

    remove unneeded vars

--- terraform/modules/codepipeline/locals.tf
+++ terraform/modules/codepipeline/locals.tf
@@ -7,6 +7,4 @@ locals {
     SvcCodeURL    = "https://github.com/alphagov/cyber-security-github-usage"
   }
 
-#   docker_hub_username = jsondecode(data.aws_secretsmanager_secret_version.dockerhub_creds.secret_string)["username"]
-#   docker_hub_password = jsondecode(data.aws_secretsmanager_secret_version.dockerhub_creds.secret_string)["password"]
 }
